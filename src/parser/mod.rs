00ead8dd6ad5b6997813730e26a6e79e26a51487
Robert Sjöblom <robert.sjoblom@fortnox.se>
2026-03-01 01:13:25 +0100

    refactor: replace glob re-exports with explicit named re-exports

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,4 +3,9 @@
 pub mod ir;
 pub(crate) mod pg_query;
 
-pub use ir::*;
+pub use ir::{
+    AlterTable, AlterTableAction, Cluster, ColumnDef, CreateIndex, CreateTable, DefaultExpr,
+    DeleteFrom, DropIndex, DropSchema, DropTable, IndexColumn, InsertInto, IrNode, Located,
+    PartitionBy, PartitionStrategy, QualifiedName, SourceSpan, TableConstraint, TablePersistence,
+    TriggerDisableScope, TruncateTable, TypeName, UpdateTable,
+};
